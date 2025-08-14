6327c541c74cee03cf7c801c08a0b35c137ba857
pelumi adeyemi <pelumiadeyemi111@gmail.com>
2025-08-14 21:44:03 +0100

    Page Completed

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,3 @@
-// This example uses pure CSS animation for looping.
-// JS is optional, but here’s an example for pausing on hover.
-
 const carouselTrack = document.querySelector(".carousel-track");
 
 carouselTrack.addEventListener("mouseenter", () => {
